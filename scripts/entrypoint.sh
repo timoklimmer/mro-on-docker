e349e92882176fbaf68bfb0bb767f1925de30ef5
Darius Tehrani <darius.tehrani@microsoft.com>
2019-05-02 19:19:55 +0200

    switching to ubuntu18, merging docker RUN operations

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-exec /usr/bin/Rscript -e "shiny::runApp(appDir='/shiny-app', host='0.0.0.0', port=80)"
+exec /usr/bin/Rscript -e "shiny::runApp(appDir='/shiny-app', host='0.0.0.0', port=8080)"
 
